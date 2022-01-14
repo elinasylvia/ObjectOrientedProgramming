79b123e45a4533f12508bbda44f0ecf8d4ef2bc4
Elina Pesari <Elina.Pesari@gmail.com>
2022-01-14 18:13:47 +0200

    Car exercise modified

---
+++ CarExercise1/Car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarExercise1
+{
+    class Car
+    {
+        public string brand;
+        public string color;
+        public double speed;
+        public Car()
+        {
+            this.brand = string.Empty;
+            this.color = string.Empty;
+            this.speed = 0;
+
+        }
+        public void AskData()
+        {
+            Console.Write("Anna auton merkki: ");
+            this.brand = Console.ReadLine();
+            Console.Write("Anna auton väri: ");
+            this.color=Console.ReadLine();
+            Console.Write("Anna auton nopeus: ");
+            this.speed = int.Parse(Console.ReadLine());
+
+
+        }
+        public void Accelerate(int change) 
+        {
+            this.speed += change;
+
+        }
+        public void Brake()
+        {
+            double speedBreak=this.speed*0.1;
+            if (this.speed >= speedBreak)
+            {
+                this.speed -= this.speed * 0.1;
+            }
+            
+        }
+        public void ShowCarInfo()
+        {
+            Console.WriteLine($"Auton merkki on {this.brand},  auton väri on {this.color} ja auton nopeus on {this.speed}");
+            
+        }
+    }
+}
--- CarExercise1/Program.cs
+++ CarExercise1/Program.cs
@@ -6,7 +6,22 @@ namespace CarExercise1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Car tehtävä");
+            Car car1 = new Car();
+            car1.AskData();
+            car1.ShowCarInfo();
+            car1.Accelerate(10);
+            car1.Brake();
+            car1.Brake();
+
+            car1.ShowCarInfo();
+            Car car2 = new Car();
+            car2.AskData();
+            car2.ShowCarInfo();
+            car2.Accelerate(10);
+            car2.Brake();
+            car2.ShowCarInfo();
+            Console.WriteLine("Car tehtävä loppui");
         }
     }
 }
