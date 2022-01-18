95f0236876e89e8cff4d6f1e8b83ad8e99174552
Elina Pesari <Elina.Pesari@gmail.com>
2022-01-18 11:14:36 +0200

    CompanyExercise created

---
+++ Company/Building.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyExercise
+{
+    class Building
+    {
+        public string address;
+        public int buildingYear;
+
+        //empty constructor
+        public Building()
+        {
+            this.address = string.Empty;
+            this.buildingYear = 0;
+        }
+
+        public Building(string address, int buildingYear)
+        {
+            this.address = address;
+            this.buildingYear = buildingYear;
+        }
+        //copy constructor
+        public Building(Building building)
+        {
+            this.address = building.address;
+            this.buildingYear = building.buildingYear;
+        }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Rakennuksen osoite: {this.address}, rakennusvuosi: {this.buildingYear}");
+        }
+
+        public void CalculateAge()
+        {
+            int age = 2022 - this.buildingYear;
+            Console.WriteLine($"Rakennuksen ikä: {age} ");
+        }
+        public override string ToString()
+        {
+            return ($"luokka:{ base.ToString()}, osoite: {this.address}, rakennusvuosi {this.buildingYear}");
+        }
+    }
+}
---
+++ Company/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CompanyExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Company exercise");
+
+            //Example
+            Console.WriteLine("Example: Building class");
+            Console.WriteLine("\n\nSchool");
+            Building school = new Building();
+            school.buildingYear = 1990;
+            school.address = "Skinnarilankatu 36, 53850 Lappeenranta";
+            school.PrintInfo();
+            Console.WriteLine("school-olio: " + school.ToString());
+            school.CalculateAge();
+
+            Console.WriteLine("\n\nOffice");
+            Building office = new Building("Marssitie 50, 53100 Lappeenranta", 2000);
+            office.PrintInfo();
+            Console.WriteLine("office-olio: " + school.ToString());
+            office.CalculateAge();
+
+
+            Console.WriteLine("\n\nPost Office");
+            // create object using copy constructor
+            Building postOffice = new Building(office);
+            postOffice.PrintInfo();
+            Console.WriteLine("postOffice-olio: " + school.ToString());
+            postOffice.CalculateAge();
+            
+
+        }
+    }
+}
