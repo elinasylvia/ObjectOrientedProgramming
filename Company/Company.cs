479ad5da8390058d80a226efc8de5fa49482eccf
Elina Pesari <Elina.Pesari@gmail.com>
2022-01-19 21:28:17 +0200

    Companyexercise finished

---
+++ Company/Company.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyExercise
+{
+    class Company
+    {
+
+        public string title;
+        public string address;
+        public int phone;
+        public double outcome;
+        public double expense;
+
+        public Company()
+        {
+            this.title = string.Empty;
+            this.address = string.Empty;
+            this.phone = 0;
+            this.outcome = 0;
+            this.expense = 0;
+
+        }
+        public Company(string title, string address, int phone, double outcome, double expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.outcome = outcome;
+            this.expense = expense;
+        }
+        public Company(Company company)
+        {
+            this.title = company.title;
+            this.address = company.address;
+            this.phone = company.phone;
+            this.outcome = company.outcome;
+            this.expense = company.expense;
+        }
+        public void CompanySummary()
+        {
+            Console.WriteLine($"Yrityksen nimi ja yhteystiedot: {this.title}, {this.address}, {this.phone}. Yrityksen tulot: {this.outcome}. Yrityksen menot: {this.expense}");
+        }
+        public void FinancialSituation()
+        {
+            double profits;
+            profits = (this.outcome - this.expense) / this.expense * 100;
+            Console.WriteLine("Yrityksen voitto: " + profits + " %");
+            if (profits < 100)
+            {
+                Console.WriteLine("Yrityksellä menee kehnosti");
+            }
+            else if (profits <= 200)
+            {
+                Console.WriteLine("Yrityksellä menee välttävästi");
+            }
+            else if (profits >= 200 && profits <300)
+            {
+                Console.WriteLine("Yrityksellä menee tyydyttävästi");
+            }
+            else if (profits >= 300)
+            {
+                Console.WriteLine("Yrityksellä menee hyvin");
+            }
+        }
+    }
+}
--- Company/Program.cs
+++ Company/Program.cs
@@ -8,30 +8,58 @@ namespace CompanyExercise
         {
             Console.WriteLine("Company exercise");
 
-            //Example
-            Console.WriteLine("Example: Building class");
-            Console.WriteLine("\n\nSchool");
-            Building school = new Building();
-            school.buildingYear = 1990;
-            school.address = "Skinnarilankatu 36, 53850 Lappeenranta";
-            school.PrintInfo();
-            Console.WriteLine("school-olio: " + school.ToString());
-            school.CalculateAge();
-
-            Console.WriteLine("\n\nOffice");
-            Building office = new Building("Marssitie 50, 53100 Lappeenranta", 2000);
-            office.PrintInfo();
-            Console.WriteLine("office-olio: " + school.ToString());
-            office.CalculateAge();
-
-
-            Console.WriteLine("\n\nPost Office");
-            // create object using copy constructor
-            Building postOffice = new Building(office);
-            postOffice.PrintInfo();
-            Console.WriteLine("postOffice-olio: " + school.ToString());
-            postOffice.CalculateAge();
+            Company kahvila = new Company("Kissakahvila / Leipomo Katinhäntä", "Valtakatu 30, 53100 Lappeenranta", 0401234567, 9546.56, 3465.09);
+            kahvila.CompanySummary();
+            kahvila.FinancialSituation();
+            Company leipomo = new Company(kahvila);
+            leipomo.CompanySummary();
+            leipomo.FinancialSituation();
+            Company kirpputori = new Company("KirppuTori", "Ratakatu 2, 53120 Lappeenranta", 097564321, 116754.34, 2436.32 );
+            kirpputori.CompanySummary();
+            kirpputori.FinancialSituation();
             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Example
+            //Console.WriteLine("Example: Building class");
+            //Console.WriteLine("\n\nSchool");
+            //Building school = new Building();
+            //school.buildingYear = 1990;
+            //school.address = "Skinnarilankatu 36, 53850 Lappeenranta";
+            //school.PrintInfo();
+            //Console.WriteLine("school-olio: " + school.ToString());
+            //school.CalculateAge();
+
+            //Console.WriteLine("\n\nOffice");
+            //Building office = new Building("Marssitie 50, 53100 Lappeenranta", 2000);
+            //office.PrintInfo();
+            //Console.WriteLine("office-olio: " + school.ToString());
+            //office.CalculateAge();
+
+
+            //Console.WriteLine("\n\nPost Office");
+            //// create object using copy constructor
+            //Building postOffice = new Building(office);
+            //postOffice.PrintInfo();
+            //Console.WriteLine("postOffice-olio: " + school.ToString());
+            //postOffice.CalculateAge();
+
 
         }
     }
