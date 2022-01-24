12bb909c05d3fad891d5effd21c93170f9f4b2f5
Elina Pesari <Elina.Pesari@gmail.com>
2022-01-24 21:06:09 +0200

    EmployeeExercise finished

---
+++ EmployeeExercise/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeExercise
+{
+    class Employee
+    {
+       public string name;
+       public int id;
+       public string position;
+       public double salary;
+
+        public Employee(string name, int id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+        }
+
+        public void CompareSalary(Employee employee)
+        {
+            if (this.salary > employee.salary)
+            {
+                Console.WriteLine($"Työntekijän {this.name} palkka on suurempi kuin työntekijän {employee.name} palkka.");
+            }
+            else Console.WriteLine($"Työntekijän {employee.name} palkka on suurempi kuin työntekijän {this.name} palkka.");
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public override string ToString()
+        {
+            return $"Työntekijän nimi: { this.name}, id: { this.id}, tehtävä: { this.position}, palkka {this.salary} euroa.";
+        }
+    }
+}
---
+++ EmployeeExercise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EmployeeExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Alustetaan taulukkoon työntekijöiden tiedot
+            Employee[] employees = new Employee[4];
+            employees[0] = new Employee("Aku Ankka" , 8182, "asentaja", 2812.40 );
+            employees[1] = new Employee("Iines Ankka", 8183, "kahvilatyöntekijä", 1716.90);
+            employees[2] = new Employee("Mikki Hiiri", 8184, "koneenkäyttäjä", 2913.70);
+            employees[3] = new Employee("Hessu Hopo", 8185, "osaston päällikkö", 3515.60);
+
+            //tulostetaan kaikkien työntekijöiden tiedot
+            foreach (Employee currentEmployee in employees)
+            {
+                currentEmployee.PrintEmployeeInfo();
+            }
+
+            Console.WriteLine();
+
+            //verrataan työntekijöiden palkkoja keskenään
+            for (int i = 0; i <employees.Length; i++)
+            {
+                int comparableI = i + 1;
+                if (comparableI == employees.Length)
+                {
+                    comparableI = 0;
+                }
+                employees[i].CompareSalary(employees[comparableI]);
+            }
+        }
+    }
+}
